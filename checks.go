5c11bb291b5f2ce3a7294d4143ecdc513d740fef
Peter Donald <peterdonald3@outlook.com>
2022-03-28 17:35:00 +0100

    ISO3166-2 initial build

---
+++ checks.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+)
+
+func convertDia(s string) string {
+	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+	s, _, _ = transform.String(t, s)
+	return s
+}
+
+// Check map for Alpha3 Match and return ISO
+// example, "gb" will return "GBR"
+func Alpha3Match(s string) *string {
+	var (
+		returnStr string
+	)
+	s = strings.ToLower(strings.TrimSpace(s))
+
+	for _, val := range isocodes {
+		if s == strings.ToLower(val.alph2) || s == strings.ToLower(val.alph3) {
+			returnStr = fmt.Sprintf("%v", val.alph3)
+			return &returnStr
+		}
+		if s == strings.ToLower(strings.TrimSpace(convertDia(val.enName))) || s == strings.ToLower(strings.TrimSpace(convertDia(val.frName))) {
+			returnStr = fmt.Sprintf("%v", val.alph3)
+			return &returnStr
+		}
+		if s == strings.ToLower(strings.TrimSpace(val.enName)) || s == strings.ToLower(strings.TrimSpace(val.frName)) {
+			returnStr = fmt.Sprintf("%v", val.alph3)
+			return &returnStr
+		}
+	}
+	return nil
+}
+
+// Check map for Alpha3 Match and return ISO
+// example, "gb" will return "GB"
+func Alpha2Match(s string) *string {
+	var (
+		returnStr string
+	)
+	s = strings.ToLower(strings.TrimSpace(s))
+	for _, val := range isocodes {
+		if s == strings.ToLower(val.alph2) || s == strings.ToLower(val.alph3) {
+			returnStr = fmt.Sprintf("%v", val.alph2)
+			return &returnStr
+		}
+		if s == strings.ToLower(strings.TrimSpace(convertDia(val.enName))) || s == strings.ToLower(strings.TrimSpace(convertDia(val.frName))) {
+			returnStr = fmt.Sprintf("%v", val.alph2)
+			return &returnStr
+		}
+		if s == strings.ToLower(strings.TrimSpace(val.enName)) || s == strings.ToLower(strings.TrimSpace(val.frName)) {
+			returnStr = fmt.Sprintf("%v", val.alph2)
+			return &returnStr
+		}
+	}
+	return nil
+}
+
+// Check map for Alpha3 Match and return ISO
+// example, "gb" will return "GB"
+func StructMatch(s string) *IsoEntry {
+	var (
+		entry IsoEntry
+	)
+	s = strings.ToLower(strings.TrimSpace(s))
+	for _, val := range isocodes {
+		if s == strings.ToLower(val.alph2) || s == strings.ToLower(val.alph3) {
+			entry = IsoEntry{
+				enName:  val.enName,
+				frName:  val.frName,
+				alph2:   val.alph2,
+				alph3:   val.alph3,
+				numCode: val.numCode,
+			}
+			return &entry
+		}
+		if s == strings.ToLower(strings.TrimSpace(convertDia(val.enName))) || s == strings.ToLower(strings.TrimSpace(convertDia(val.frName))) {
+			entry = IsoEntry{
+				enName:  val.enName,
+				frName:  val.frName,
+				alph2:   val.alph2,
+				alph3:   val.alph3,
+				numCode: val.numCode,
+			}
+			return &entry
+		}
+		if s == strings.ToLower(strings.TrimSpace(val.enName)) || s == strings.ToLower(strings.TrimSpace(val.frName)) {
+			entry = IsoEntry{
+				enName:  val.enName,
+				frName:  val.frName,
+				alph2:   val.alph2,
+				alph3:   val.alph3,
+				numCode: val.numCode,
+			}
+			return &entry
+		}
+
+	}
+	return nil
+}
---
+++ checks_test.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+var (
+	Nil       *string
+	NilStruct *IsoEntry
+
+	GBR       string = isocodes["GB"].alph3
+	GB        string = isocodes["GB"].alph2
+	GBRStruct        = IsoEntry{
+		enName:  isocodes["GB"].enName,
+		frName:  isocodes["GB"].frName,
+		alph2:   isocodes["GB"].alph2,
+		alph3:   isocodes["GB"].alph3,
+		numCode: isocodes["GB"].numCode,
+	}
+
+	USA       string = isocodes["US"].alph3
+	US        string = isocodes["US"].alph2
+	USAStruct        = IsoEntry{
+		enName:  isocodes["US"].enName,
+		frName:  isocodes["US"].frName,
+		alph2:   isocodes["US"].alph2,
+		alph3:   isocodes["US"].alph3,
+		numCode: isocodes["US"].numCode,
+	}
+
+	ALA       string = isocodes["AX"].alph3
+	AX        string = isocodes["AX"].alph2
+	ALAStruct        = IsoEntry{
+		enName:  isocodes["AX"].enName,
+		frName:  isocodes["AX"].frName,
+		alph2:   isocodes["AX"].alph2,
+		alph3:   isocodes["AX"].alph3,
+		numCode: isocodes["AX"].numCode,
+	}
+
+	BHR       string = isocodes["BH"].alph3
+	BH        string = isocodes["BH"].alph2
+	BHRStruct        = IsoEntry{
+		enName:  isocodes["BH"].enName,
+		frName:  isocodes["BH"].frName,
+		alph2:   isocodes["BH"].alph2,
+		alph3:   isocodes["BH"].alph3,
+		numCode: isocodes["BH"].numCode,
+	}
+)
+
+func TestAlpha3Match(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *string
+	}{
+		{"GBR Test", args{s: "gb"}, &GBR},
+		{"USA upper Test", args{s: "USA"}, &USA},
+		{"Nothing Test", args{s: ""}, Nil},
+		{"Åland Islands Test", args{s: "Aland Islands"}, &ALA},
+		{"Åland Islands Test", args{s: "Åland Islands"}, &ALA},
+		{"Bahreïn Test", args{s: "Bahreïn"}, &BHR},
+		{"Bahreïn Test (FR)", args{s: "Bahrein"}, &BHR},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Alpha3Match(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Alpha3Match() = %#v, want %#v", *got, *tt.want)
+			}
+		})
+	}
+}
+
+func TestAlpha2Match(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *string
+	}{
+		{"GBR Test", args{s: "gb"}, &GB},
+		{"USA upper Test", args{s: "USA"}, &US},
+		{"Nothing Test", args{s: ""}, Nil},
+		{"Åland Islands Test", args{s: "Aland Islands"}, &AX},
+		{"Åland Islands Test", args{s: "Åland Islands"}, &AX},
+		{"Bahreïn Test", args{s: "Bahreïn"}, &BH},
+		{"Bahreïn Test (FR)", args{s: "Bahrein"}, &BH},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Alpha2Match(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Alpha3Match() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStructMatch(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *IsoEntry
+	}{
+
+		{"GBR Test", args{s: "gb"}, &GBRStruct},
+		{"USA upper Test", args{s: "USA"}, &USAStruct},
+		{"Nothing Test", args{s: ""}, NilStruct},
+		{"Åland Islands Test", args{s: "Aland Islands"}, &ALAStruct},
+		{"Åland Islands Test", args{s: "Åland Islands"}, &ALAStruct},
+		{"Bahreïn Test", args{s: "Bahreïn"}, &BHRStruct},
+		{"Bahreïn Test (FR)", args{s: "Bahrein"}, &BHRStruct},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := StructMatch(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("StructMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
